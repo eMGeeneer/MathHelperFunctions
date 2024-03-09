79302ae9dc80cc2e5076739dd40945d00e1de972
Destitution <145163394+em-gee2@users.noreply.github.com>
2024-03-08 22:50:34 -0500

    Update MathHelperFunctions.cpp

--- MathHelperFunctions.cpp
+++ MathHelperFunctions.cpp
@@ -2,11 +2,13 @@
 #include <math.h>
 
 // returns two doubles: an estimation of sqrt(2) and its error bounds
+// this uses a recursive math function similar to the Fibonacci sequence to estimate
+// it has a numerator and a denominator
 double* sqrt2(int x) {
-    long n[] = {1, 3};
-    long d[] = {1, 2};
+    long n[] = {1, 3}; // the starting values of the numerator
+    long d[] = {1, 2}; // starting values of the denominator
     for (int i = 0; i < x; i++) {
-        n[i % 2] += 2 * n[(i + 1) % 2];
+        n[i % 2] += 2 * n[(i + 1) % 2]; // how each subsequent value is defined
         d[i % 2] += 2 * d[(i + 1) % 2];
     }
     double estimate = (double) n[x % 2] / (double) d[x % 2];
@@ -31,6 +33,7 @@ signed char getNumBits(unsigned long n) {
 }
 
 // returns the greatest unsigned long less than a given unsigned long's square root 
+// uses binary search
 unsigned long fastIntSqrt(unsigned long n) {
     if (n < 2) {
         return n;
