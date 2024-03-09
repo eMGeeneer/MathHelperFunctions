c1f002df0340aab3746d99ce89ab12840aea2a84
Destitution <145163394+em-gee2@users.noreply.github.com>
2024-03-08 22:12:22 -0500

    Update MathHelperFunctions.cpp

--- MathHelperFunctions.cpp
+++ MathHelperFunctions.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <stdio.h>
 #include <math.h>
 
+// returns two doubles: an estimation of sqrt(2) and its error bounds
 double* sqrt2(int x) {
     long n[] = {1, 3};
     long d[] = {1, 2};
@@ -11,6 +11,43 @@ double* sqrt2(int x) {
     }
     double estimate = (double) n[x % 2] / (double) d[x % 2];
     return new double[2]{estimate, fabs((double) n[(x + 1) % 2] / (double) d[(x + 1) % 2] - estimate)};
+}
+
+signed char getNumBits(unsigned long n) {
+    signed char x = 0;
+    if (n == 0) {
+        return x;
+    }
+    unsigned long compare = 1u;
+    for (unsigned int i = 32; i > 0; i /= 2) {
+        compare = (compare << i) - 1;
+        if (n <= compare) {
+            x += i;
+            n <<= i;
+        }
+    }
+    return 64 - x;
+}
+
+unsigned long fastIntSqrt(unsigned long n) {
+    unsigned long max = (getNumBits(n) + 1) / 2;
+    unsigned long min = 1 << max - 1;
+    max = 1 << max;
+    unsigned long x = (max + min) / 2;
+    unsigned long sq = x * x;
+    while(max > min && sq != n) {
+        if (sq > n) {
+            max = x - 1;
+            x = (max + min) / 2;
+            sq = x * x;
+        }
+        else if (sq < n) {
+            min = x + 1;
+            x = (max + min) / 2;
+            sq = x * x;
+        }
+    }
+    return x;
 }
 
 int main() {
