8e6ad70947de175c8186d5c021dfe5fca5cc48b3
Emgeeneer <145163394+eMGeeneer@users.noreply.github.com>
2024-04-26 04:24:46 -0400

    Update MathHelperFunctions.cpp

--- MathHelperFunctions.cpp
+++ MathHelperFunctions.cpp
@@ -42,13 +42,15 @@ unsigned long fastIntSqrt(unsigned long n) {
     max = (1 << max) - 1;
     unsigned long x = (max + min) / 2;
     unsigned long sq = x * x;
-    for(; max > min && sq != n; x = (max + min) / 2, sq = x * x) {
+    while(max > min && sq != n) {
         if (sq > n) {
             max = x - 1;
         }
         else if (sq < n) {
             min = x + 1;
         }
+        x = (max + min) / 2; 
+        sq = x * x;
     }
     if (sq > n) {
         x--;
